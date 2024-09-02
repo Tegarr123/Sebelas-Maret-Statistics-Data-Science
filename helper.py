0d19e86d4b184c89f1fe035d1a011f0a72930a44
fadhino <fadhino987@gmail.com>
2024-09-02 14:24:48 +0700

    update helper

--- helper.py
+++ helper.py
@@ -3,3 +3,18 @@ import re
 def rename_column(df:pd.DataFrame):
     df.columns = list(map(lambda s: re.sub(r'\(.*\)', '', s), df.columns))
     df.columns = list(map(lambda s : s.strip(), df.columns))
+def fix_value(df:pd.DataFrame):
+    df.index = df['ID']
+    df['time'] = df['time'].apply(lambda x : pd.Timestamp(re.sub('T', ' ', x)))
+    cat_val, _ = get_cat_num(df)
+    for cat_col in cat_val:
+        not_NaN_idx = ~df[cat_col].isna()
+        df[cat_col][not_NaN_idx] = df[cat_col][not_NaN_idx].apply(lambda x : re.sub(',','.',x)).astype(float)
+
+
+
+
+def get_cat_num(df:pd.DataFrame):
+    cat_val = [i for i in df.columns if df[i].dtype == 'object']
+    num_val = [i for i in df.columns if df[i].dtype != 'object']
+    return cat_val, num_val
