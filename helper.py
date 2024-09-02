5ca40a3be5a44226f0a65047c0f862bce5867870
fadhino <fadhino987@gmail.com>
2024-09-02 11:11:52 +0700

    update helper

--- helper.py
+++ helper.py
@@ -1,9 +1,5 @@
 import pandas as pd
 import re
-def rename_column(train_df:pd.DataFrame, test_df:pd.DataFrame=None):
+def rename_column(train_df:pd.DataFrame):
     train_df.columns = list(map(lambda s: re.sub(r'\(.*\)', '', s), train_df.columns))
     train_df.columns = list(map(lambda s : s.strip(), train_df.columns))
-
-    if test_df != None:
-        test_df.columns = list(map(lambda s: re.sub(r'\(.*\)', '', s), test_df.columns))
-        test_df.columns = list(map(lambda s : s.strip(), test_df.columns)) 
